64a579e247de0c519e13856f607ce4f46fe6cfb3
MinSeok <kjwl00219@gmail.com>
2019-11-06 23:46:17 +0900

    [INITIAL]Google Map API
    [INITIAL]API_Practice
    [ADD]Google Map API

---
+++ GoogleMapAPI/app/src/main/java/com/example/googlemapapi/MapsActivity.kt
@@ -0,0 +1,45 @@
+package com.example.googlemapapi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        //구글 맵 객체 불러오기!
+        mMap = googleMap
+
+        // 마커 위치 설정 Add a marker in Sydney and move the camera
+        //val sydney = LatLng(-34.0, 151.0)
+        val dsm = LatLng(36.391442, 127.363347)
+
+        mMap.addMarker(MarkerOptions().position(dsm).title("대마고에 마커를 표시했습니다!")) //Marker in Sydney
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(dsm))
+    }
+}
