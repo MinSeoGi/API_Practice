6e34c0f06063d2f40a41a0b762c93b609549ef9f
MinSeok <kjwl00219@gmail.com>
2019-11-13 10:28:50 +0900

    [Update]Current Location
    Update Current Location

--- GoogleMapAPI/app/src/main/java/com/example/googlemapapi/MapsActivity.kt
+++ GoogleMapAPI/app/src/main/java/com/example/googlemapapi/MapsActivity.kt
@@ -1,35 +1,74 @@
 package com.example.googlemapapi
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.yesButton
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: MyLocationCallBack
+
+    private val REQUEST_ACCESS_FINE_LOCATION = 1000
+
+    private val polylineOptions = PolylineOptions().width(5f).color(Color.RED)
+    private lateinit var lastLocation: Location
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //화면 안꺼지게 하기
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //SupportMapFragment 가져와서 지도 준비되면 알림을 받음
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        locationInit()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    private fun locationInit() {
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        locationCallback = MyLocationCallBack()
+
+        locationRequest = LocationRequest()
+
+        //GPS 우선
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        locationRequest.interval = 10000
+
+        locationRequest.fastestInterval = 5000
+    }
 
     override fun onMapReady(googleMap: GoogleMap) {
         //구글 맵 객체 불러오기!
@@ -41,5 +80,119 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mMap.addMarker(MarkerOptions().position(dsm).title("대마고에 마커를 표시했습니다!")) //Marker in Sydney
         mMap.moveCamera(CameraUpdateFactory.newLatLng(dsm))
+
+        // 1
+        //mMap.isMyLocationEnabled = true
+
+// 2
+//        fusedLocationProviderClient.lastLocation.addOnSuccessListener(this) { location ->
+//            // Got last known location. In some rare situations this can be null.
+//            // 3
+//            if (location != null) {
+//                lastLocation = location
+//                val currentLatLng = LatLng(location.latitude, location.longitude)
+//                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+//            }
+//        }
+
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        //권한요청
+        permissionCheck(cancel = {
+            showPermissionInfoDialog()
+        }, ok = {
+            //현재 위치를 주기적으로 요청
+            addLocationListener()
+        })
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun addLocationListener() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    inner class MyLocationCallBack : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            val location = locationResult?.lastLocation
+
+            location?.run {
+                //14 level로 확대하고 현재 위치로 카메라 이동
+                val latLng = LatLng(latitude, longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
+
+                Log.d("MapsActivity", "위도: $latitude, 경도: $longitude")
+
+//                polylineOptions.add(latLng)
+//                mMap.addPolyline(polylineOptions)
+
+                //자기위치 표시
+                mMap.isMyLocationEnabled = true
+            }
+        }
+    }
+
+    private fun permissionCheck(cancel: () -> Unit, ok:() -> Unit) {
+        //위치 권한 있는지 검사
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            //권한 허용 X
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                //이전에 권한을 한번 거부한 적이 있을때 실행
+                cancel()
+            } else {
+                //권한 요청
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_ACCESS_FINE_LOCATION)
+            }
+        } else {
+            //권한 수락했을때 실행
+            ok()
+        }
+    }
+
+    private fun showPermissionInfoDialog() {
+        alert("현재 위치 정보를 얻으려면 위치 권한이 필요합니다", "권한이 필요한 이유") {
+            yesButton {
+                ActivityCompat.requestPermissions(this@MapsActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_ACCESS_FINE_LOCATION)
+            }
+            noButton { }
+        }.show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_ACCESS_FINE_LOCATION -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    //권한 허용 시
+                    addLocationListener()
+                } else {
+                    //권한 거부 시
+                    toast("권한 거부 됨")
+                }
+                return
+            }
+        }
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+
+        removeLocationLister()
+    }
+
+    private fun removeLocationLister() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
+
 }
